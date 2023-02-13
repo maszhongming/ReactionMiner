e0af14e3de3d2fc25381f29520110e434d60f489
Bobby Zhou <bobbyzhou22@gmail.com>
2023-02-12 23:48:38 -0600

    lots of improvements

--- xmlParser.py
+++ xmlParser.py
@@ -1,13 +1,61 @@
 import xml.etree.ElementTree as ET
+import json
 
 tree = ET.parse("./acs.joc.5b00443.xml")
 root = tree.getroot()
 
-allText = ''
-for word in root.iter('Word'):
-    wordBuf = ''
-    for char in word.iter('Char'):
-        wordBuf += str(char.text)
-    allText += wordBuf + ' '
+output = {}
+output["fullText"] = ""
+output["title & info"] = ""
 
-print(allText)
+sectionTitleBuf = ""  # keeps track of new section title, empty while populating section
+currSection = "title & info"
+
+ignoredSections = [
+    "The Journal of Organic Chemistry",
+    "pubs.acs.org/joc",
+    "*",
+    "Received:",
+    "Published:",
+]
+
+weirdChar = {
+    "\u2212": "-",
+    "\ufb00": "ff",
+    "\ufb01": "fi",
+    "\ufb02": "fl",
+    "fraction(-)": "",
+    "\u25a0": "",
+}
+for word in root.iter("Word"):
+    wordBuf = ""
+    for char in word.iter("Char"):
+
+        # writing section title
+        if char.attrib["RGB"] == "[1.0]":
+            if char.text != "\u25a0":
+                sectionTitleBuf += str(char.text)
+        # create new section
+        elif sectionTitleBuf:
+            if sectionTitleBuf.strip() in ignoredSections:
+                sectionTitleBuf = ""
+            else:
+                output[currSection] = output[currSection][
+                    : -len(sectionTitleBuf) - 1
+                ]  # strip the end
+                sectionTitleBuf = sectionTitleBuf.strip().strip(":").lower()
+                output[sectionTitleBuf] = ""
+                currSection = sectionTitleBuf
+                sectionTitleBuf = ""
+        if char.text in weirdChar:
+            wordBuf += weirdChar[str(char.text)]
+            continue
+        wordBuf += str(char.text)
+    output["fullText"] += wordBuf + " " if wordBuf else ""
+    output[currSection] += wordBuf + " " if wordBuf else ""
+    if sectionTitleBuf:
+        sectionTitleBuf += " "
+
+# print(output["allText"])
+with open("sample.json", "w") as outfile:
+    json.dump(output, outfile)
