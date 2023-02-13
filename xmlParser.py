a72fe66fdfb25c7390dfc18d37657f1976e3db17
Bobby Zhou <bobbyzhou22@gmail.com>
2023-02-13 00:00:13 -0600

    fixed spacing for hyphen-connected words, ignored side notes, prettified output json file

--- xmlParser.py
+++ xmlParser.py
@@ -31,6 +31,10 @@ for word in root.iter("Word"):
     wordBuf = ""
     for char in word.iter("Char"):
 
+        # ignore side vertical notes
+        location = char.attrib["BBOX"].split(" ")[0]
+        if location in ["9.0", "18.0"]:
+            continue
         # writing section title
         if char.attrib["RGB"] == "[1.0]":
             if char.text != "\u25a0":
@@ -51,11 +55,13 @@ for word in root.iter("Word"):
             wordBuf += weirdChar[str(char.text)]
             continue
         wordBuf += str(char.text)
-    output["fullText"] += wordBuf + " " if wordBuf else ""
-    output[currSection] += wordBuf + " " if wordBuf else ""
+    output["fullText"] += wordBuf
+    output["fullText"] += " " if wordBuf and wordBuf[-1] != "-" else ""
+    output[currSection] += wordBuf
+    output[currSection] += " " if wordBuf and wordBuf[-1] != "-" else ""
     if sectionTitleBuf:
         sectionTitleBuf += " "
 
 # print(output["allText"])
 with open("sample.json", "w") as outfile:
-    json.dump(output, outfile)
+    json.dump(output, outfile, indent=4)
